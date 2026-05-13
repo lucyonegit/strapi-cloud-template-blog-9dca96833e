8fe47763fc354e365528355a7a96723583e2ea24
hanlujie <lujie@bantouyan.com>
2026-05-13 14:43:24 +0800

    chore: update security middleware configuration to include custom CSP directives for Cloudinary media support

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,29 @@
-module.exports = [
+export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
